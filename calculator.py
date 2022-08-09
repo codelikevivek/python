efbae2b38bca53ca0a06ab3d17891e9bbd64afaf
Vivek Sharma <110883929+codelikevivek@users.noreply.github.com>
2022-08-09 22:25:58 +0530

    Calculator Using Python

---
+++ calculator.py
@@ -0,0 +1,19 @@
+first = input("Enter First Digit: ")
+operator = input("Enter Operator (+, -, *, / %) : ")
+second = input("Enter Second Digit: ")
+
+first = int(first)
+second = int(second)
+
+if operator == '+':
+    print(first + second)
+elif operator == '-':
+    print(first - second)
+elif operator == '*':
+    print(first * second)
+elif operator == '/':
+    print(first / second)
+elif operator == '%':
+    print(first % second)
+else:
+    print("Invalid operator")
